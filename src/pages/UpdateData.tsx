740f14169e94b1b555ea1dbf62620d2368343da1
escolaSantaCorona <118981045+escolaSantaCorona@users.noreply.github.com>
2023-08-22 17:12:26 -0300

    Update UpdateData.tsx

--- src/pages/UpdateData.tsx
+++ src/pages/UpdateData.tsx
@@ -43,8 +43,6 @@ export default function UpdateAlunos() {
   const { register, setValue, handleSubmit, reset, control } =
     useForm<FormDataAlunos>({});
 
-  // ...
-
   // função para preencher os dados do aluno
   const fillFormWithStudentData = (name: string) => {
     const student = data.find((student) => student.nome_do_aluno === name);
@@ -281,3 +279,53 @@ export default function UpdateAlunos() {
     </Container>
   );
 }
+
+
+
+/*
+Explicação:
+1) Criar uma variavel para procurar o nome do aluno
+2) Se o aluno existe, fazer um loop dentro do objeto para setar cada campo input com o valor do objeto.
+3) cada campo tem um  { label: "Nome da Mãe", id: "mae" }, o id é o nome do campo.
+4) O metodo Object.entries cria uma par de chaves [["mae","Rita"],["pai","Silva"]] a chave(key) 
+é o primeiro elemento do array e o valor (value) é o segundo.
+5) nessa parte "setValue(key as keyof FormDataAlunos, value);" o metodo keyof garante que a chave tem que ser do tipo FormDataAlunos.
+e seta o valor do input com o valor que corresponde a chave.
+
+const fillFormWithStudentData = (name: string) => {
+    const student = data.find((student) => student.nome_do_aluno === name);
+
+    if (student) {
+      // loop through the student data object and set the value of each field
+      Object.entries(student).forEach(([key, value]) => {
+        if (
+          key === "data_de_nascimento" ||
+          key === "data_movimento" ||
+          key === "data_matricula"
+        ) {
+          // formata a data antes de definir o valor
+          setValue(key as keyof FormDataAlunos, formatDate(value));
+        } else {
+          setValue(key as keyof FormDataAlunos, value);
+        }
+      });
+    } else {
+      console.log(`Could not find student with name "${name}"`);
+    }
+  };
+----------------------------------------------------------------------------------
+7) Para usar a função anterior nos inputs usa-se o codigo:
+ {CamposdeMovimentacaoEscolarToUpdate.map(({ label, id }) => (
+                <Grid item xs={12} sm={6} key={id}>
+                    <TextField
+                      fullWidth
+                      id={id}
+                      variant="standard"
+                      sx={{
+                        borderRadius: "4px",
+                      }}
+                     
+                      {...register(id as keyof FormDataAlunos)}
+                    />
+8) o register do rect hook form( que registra o valor, recebe o id que é do tipo FormDataAlunos.
+*/
