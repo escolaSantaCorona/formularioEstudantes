0f092cdb81bcd2d7b371bdd06582c1e4b57b93bd
escolaSantaCorona <118981045+escolaSantaCorona@users.noreply.github.com>
2024-01-03 10:45:39 -0300

    Update getAlunosData.tsx

--- src/components/doc/getAlunosData.tsx
+++ src/components/doc/getAlunosData.tsx
@@ -90,7 +90,7 @@ import { useData } from "@/context/Context";
                         {aluno.nome_do_aluno}
                       </StyledTableCell>
                       <StyledTableCell align="center">
-                        {aluno.turma_em_2023}
+                        {aluno.turma_em_2024}
                       </StyledTableCell>
                       <StyledTableCell align="center">
                         {String(aluno.turno)}
